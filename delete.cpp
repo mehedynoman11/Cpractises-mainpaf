6a89034a3a71104f8e8cabc7e72bbf1ab80be63c
mehedynoman11 <mehedynoman11@gmail.com>
2025-05-14 19:35:05 +0600

    insert at tail

--- delete.cpp
+++ delete.cpp
@@ -19,6 +19,45 @@ void print_linked_list(Node* head) {
     }
     cout<<endl;
 }
+void insert_any_position(Node *head, int pos, int val) {
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for(int i=1;i<=pos-1;i++) {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+
+    cout<<endl;
+}
+int size(Node* head) {
+    Node* tmp = head;
+    int count = 0;
+    while(tmp != NULL) {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+void insert_head(Node *&head, int val) {
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+void delete_node(Node* head, int pos) {
+    Node* tmp = head;
+    for(int i=1;i<=pos-1;i++) {
+        tmp = tmp->next;
+    }
+    Node* deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteNode;
+}
+void delete_head(Node*& head) {
+    Node* deleteHead = head;
+    head = head->next;
+    delete deleteHead;
+}
 int main() {
     Node* head = new Node(10);
     Node* a = new Node(20);
@@ -30,6 +69,18 @@ int main() {
     a->next = b;
     b->next = c;
     c->next = d;
+    print_linked_list(head);
+    int val, pos;
+    cin>>pos;
+    // if (pos > size(head)) {
+    //     cout<<"Invalid index"<<endl;
+    // } else if (pos == 1) {
+    //     insert_head(head, val);
+    // } else {
+    //     // insert_any_position(head, pos, val);
+    //     delete_node(head, pos);
+    // }
+    delete_head(head);
     print_linked_list(head);
     return 0;
 }
