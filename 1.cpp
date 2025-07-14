e417e6f0db62f49c65ed4ee575fa1a8c5595bf91
mehedynoman11 <mehedynoman11@gmail.com>
2025-07-14 12:30:47 +0600

    new signly list test

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
-int main () {
+#include <iostream>
+using namespace std;
+int main() {
     int n=4;
     cout<<n;
+
+    return 0;
 }
--- insert1.cpp
+++ insert1.cpp
@@ -28,18 +28,59 @@ void print_linked_list(Node* head) {
     }
     cout<<endl;
 }
+
+void insert_any_position(Node* head, int pos, int val) {
+    Node *newNode = new Node(val);
+    Node* tmp = head;
+    for(int i=0;i<pos-1;i++) {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+
+void insert_at_head(Node *head, int val){
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+
+}
 int main() {
-    Node* head = NULL;
-    Node* tail = NULL;
-    int val;
-    while (true) {
-        cin>>val;
-        if(val==-1){
-            break;
-        }
-        insert_at_tail(head,tail,val);
+    // Node* head = NULL;
+    // Node* tail = NULL;
+    // int val, pos;
+    // while (true) {
+    //     cin>>val;
+    //     if(val==-1){
+    //         break;
+    //     }
+    //     insert_at_tail(head,tail,val);
+    // }
+    // print_linked_list(head);
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    print_linked_list(head);
+    int val, pos;
+    cout<<"Enter a position"<<endl;
+    cin>>pos;
+    cout<<"Enter a position"<<endl;
+    cin>>val;
+    if (pos > sizeof(head)) {
+        cout<<"Invalid index"<<endl;
+    } else if (pos == 1) {
+        insert_at_head(head, val); 
+    }
+    else {    
+        insert_any_position(head, pos, val);
     }
     print_linked_list(head);
-
     return 0;
 }
